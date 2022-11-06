13564c9caea3990656c7a37b9a27c0eb917058a6
PaULah88 <paulaiglesiasamigo@gmail.com>
2022-11-06 13:48:13 +0100

    fix-translates

--- schema.js
+++ schema.js
@@ -1,62 +1,57 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const userSchema = Joi.object().keys({
     username: Joi.string()
-                .min(2)
-                .max(30)
-                .required()
-                .error((errors) => {
-                    switch (errors[0].code) {
-                        case 'any.required':
-                        case 'sting.required':
-                            return new Error(
-                                `El campo username es requerido`
-                            );
+        .min(2)
+        .max(30)
+        .required()
+        .error((errors) => {
+            switch (errors[0].code) {
+                case 'any.required':
+                case 'sting.required':
+                    return new Error(`Username field is required`);
 
-                        default:
-                            return new Error(
-                                `El campo username debe tener entre 2 y 30 caracteres`
-                            );    
-                    }
-                }),
+                default:
+                    return new Error(
+                        `Username field must be between 2 and 30 characters`
+                    );
+            }
+        }),
 
     email: Joi.string()
-             .email()
-             .required()
-             .error((errors) => {
-                if(errors[0].code === 'any.required') {
+        .email()
+        .required()
+        .error((errors) => {
+            if (errors[0].code === 'any.required') {
+                return new Error('Email field required');
+            } else {
+                return new Error('Wrong Email');
+            }
+        }),
 
-                    return new Error('El campo emial es requerido');
-
-                } else {
-
-                    return new Error('El email no es correcto');
-                }
-             }),
-             
     password: Joi.string()
-               .min(6)
-               .max(30)
-               .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[¡!$%^&*()_+|~=`{}:";'<>¿?,.])[a-zA-Z0-9¡!$%^&*()_+|~=`{}:";'<>¿?,.]{6,30}$/)
-               .required()
-               .error((errors) => {
-                 switch (errors[0].code) {
-                     case 'any.required':
-                         return new Error('La constraseña es requerida');
-     
-                     case 'string.pattern.base':
-                         return new Error(
-                             'La contraseña debe tener al menos una letra mayúscula, una letra minúscula, un número y un signo de puntuación'
-                         ); 
-     
-                     default:
-                         return new Error(
-                             'La constraseña debe tener entre 6 y 30 caracteres'
-                         );
-                 }
-             })         
+        .min(6)
+        .max(30)
+        .regex(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[¡!$%^&*()_+|~=`{}:";'<>¿?,.])[a-zA-Z0-9¡!$%^&*()_+|~=`{}:";'<>¿?,.]{6,30}$/
+        )
+        .required()
+        .error((errors) => {
+            switch (errors[0].code) {
+                case 'any.required':
+                    return new Error('Password is required');
 
- 
-});            
+                case 'string.pattern.base':
+                    return new Error(
+                        'Password must have at least one uppercase letter, one lowercase letter, one number, and one punctuation mark'
+                    );
+
+                default:
+                    return new Error(
+                        'Password must be between 6 and 30 characters'
+                    );
+            }
+        }),
+});
 
 module.exports = userSchema;
